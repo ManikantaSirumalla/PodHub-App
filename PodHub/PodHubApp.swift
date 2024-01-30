74347f93b24939bae614111fac9c4f6003cd8102
Manikanta Sirumalla <manikanta.sirumalla@outlook.com>
2024-01-30 23:11:08 +0530

    Initial Commit

---
+++ PodHub/PodHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodHubApp.swift
+//  PodHub
+//
+//  Created by Manikanta Sirumalla on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PodHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
